bdd160fc67458b0c20e46a6d28256aede7d0a634
Andrew Po <ensimgen@gmail.com>
2022-04-30 17:22:55 +0300

    Added task and storage models.

--- To-Do Manager/Model/Task.swift
+++ To-Do Manager/Model/Task.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+// Task's type
+enum TaskPriority {
+    case normal
+    case important
+}
+
+// Task's condition/status
+enum TaskStatus {
+    case planned
+    case completed
+}
+
+protocol TaskProtocol {
+    var title: String { get set }
+    var type: TaskPriority { get set }
+    var status: TaskStatus { get set }
+}
+
+struct Task: TaskProtocol {
+    var title: String
+    var type: TaskPriority
+    var status: TaskStatus
+}
---
+++ To-Do Manager/Model/TaskStorage.swift
@@ -0,0 +1,29 @@
+//
+//  TaskStorage.swift
+//  To-Do Manager
+//
+//  Created by Dreik on 4/30/22.
+//
+
+import Foundation
+
+protocol TasksStorageProtocol {
+    func loadTasks() -> [TaskProtocol]
+    func saveTasks(_ tasks: [TaskProtocol])
+}
+
+class TasksStorage: TasksStorageProtocol {
+    func loadTasks() -> [TaskProtocol] {
+        let testTasks: [TaskProtocol] = [
+            Task(title: "Buy break", type: .normal, status: .planned),
+            Task(title: "Wash a cat", type: .important, status: .planned),
+            Task(title: "Return the loat to Arnold", type: .important, status: .completed),
+            Task(title: "Buy a new vacuum cleaner", type: .normal, status: .completed),
+            Task(title: "Give floweres to my wife", type: .important, status: .planned),
+            Task(title: "Call to parents", type: .important, status: .planned)
+        ]
+        
+        return testTasks
+    }
+    func saveTasks(_ tasks: [TaskProtocol]) {}
+}
