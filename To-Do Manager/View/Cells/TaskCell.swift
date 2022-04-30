4df5c3130c1e272d96648a115b8e4ed77fb8d669
Andrew Po <ensimgen@gmail.com>
2022-04-30 18:39:44 +0300

    Added connections between prototypes cell and class.

---
+++ To-Do Manager/View/Cells/TaskCell.swift
@@ -0,0 +1,23 @@
+//
+//  TaskCell.swift
+//  To-Do Manager
+//
+//  Created by Dreik on 4/30/22.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
