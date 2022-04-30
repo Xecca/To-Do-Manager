ad75d5259488d49622f05741809b42b0f9f9c881
Andrew Po <ensimgen@gmail.com>
2022-04-30 17:10:37 +0300

    First commit.

---
+++ To-Do Manager/Model/Task.swift
@@ -0,0 +1,8 @@
+//
+//  Task.swift
+//  To-Do Manager
+//
+//  Created by Dreik on 4/30/22.
+//
+
+import Foundation
---
+++ To-Do Manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To-Do Manager
+//
+//  Created by Dreik on 4/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
